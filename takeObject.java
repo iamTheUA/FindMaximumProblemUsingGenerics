c6c3d2a48f8cfcbae9f738a721ec89f533c5903d
Ujjwal Aakash <ujjwalaakash46ua@gmail.com>
2021-08-09 13:00:58 +0530

    Method testMax for more obj

--- takeObject.java
+++ takeObject.java
@@ -34,13 +34,29 @@ class takeObject implements Comparable<takeObject> {
 
 	}
 
-	public static void testMax(takeObject e1, takeObject e2, takeObject e3) {
-		if (e1.compareTo(e2) == 1 && e1.compareTo(e3) == 1) {
-			System.out.println("e1 is Greatest");
-		} else if (e2.compareTo(e1) == 1 && e2.compareTo(e3) == 1) {
-			System.out.println("e2 is Greatest");
-		} else {
-			System.out.println("e3 is Greatest");
+	public static void testMax(takeObject ...e) {
+		takeObject temp;
+		for(int i=0;i<e.length;i++) {
+			for (int j = i ; j<e.length; j++) {
+				if(e[i].compareTo(e[j]) == -1 ) {
+					temp = e[i];
+					e[i]=e[j];
+					e[j]=temp;
+				}
+			}
 		}
+		
+		if(e[0].i==null) {
+			System.out.println(e[0].st);
+		}
+		else
+			System.out.println(e[0].i);
+//		if (e1.compareTo(e2) == 1 && e1.compareTo(e3) == 1) {
+//			System.out.println("e1 is Greatest");
+//		} else if (e2.compareTo(e1) == 1 && e2.compareTo(e3) == 1) {
+//			System.out.println("e2 is Greatest");
+//		} else {
+//			System.out.println("e3 is Greatest");
+//		}
 	}
 }
