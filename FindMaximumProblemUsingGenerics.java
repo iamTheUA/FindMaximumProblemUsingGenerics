d8ea74e3fc10d2fa0461b545a5909c3e5aca55b4
Ujjwal Aakash <ujjwalaakash46ua@gmail.com>
2021-08-09 11:40:57 +0530

    Making Float

--- FindMaximumProblemUsingGenerics.java
+++ FindMaximumProblemUsingGenerics.java
@@ -3,18 +3,18 @@ package Day13;
 public class FindMaximumProblemUsingGenerics {
 
 	public static void main(String[] args) {
-		Integer num1=10;
-		Integer num2=6;
-		Integer num3=5;
+		Float num1=10.5f;
+		Float num2=12.6f;
+		Float num3=34.5f;
 		
 		if(num1.compareTo(num2)==1 && num1.compareTo(num3)==1) {
-			System.out.println(num1+"is Greatest");
+			System.out.println(num1+" is Greatest");
 		}
 		else if(num2.compareTo(num1)==1 && num2.compareTo(num3)==1) {
-			System.out.println(num2+"is Greatest");
+			System.out.println(num2+" is Greatest");
 		}
 		else {
-			System.out.println(num3+"is Greatest");
+			System.out.println(num3+" is Greatest");
 		}
 	}
 
